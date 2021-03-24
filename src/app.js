f277b1ce5d71c9d49b6cd6ee70d83e2e6abbf8a1
Talita <talita.silveira@kroton.com.br>
2021-03-24 17:20:23 -0300

    ajuste no script

--- src/app.js
+++ src/app.js
@@ -1,43 +1,6 @@
 import express from 'express';
 import './database';
 import routes from './routes';
-// import session from 'express-session';
-// import dotenv from 'dotenv';
-// import envalid from 'envalid';
-// import * as oauth2 from 'oauth2-api-azure';
-// import { IAuthSettings, SecurityStrategies } from 'oauth2-api-azure';
-
-// dotenv.config();
-
-// const env = envalid.cleanEnv(process.env, {
-//   TENANT_ID: envalid.str({
-//     example: 'd197a05e-...',
-//     desc: 'Azure AD tenant name or ID. e.g. contoso.onmicrosoft.com',
-//   }),
-//   CLIENT_ID: envalid.str({
-//     example: 'd197a05e-...',
-//     desc:
-//       'The APP ID of the client app that was registered and configured to have access to the Azure resource',
-//   }),
-//   CLIENT_SECRET: envalid.str({
-//     example: 'd197a05e-...',
-//     desc: 'The Client Secret of the Client App above',
-//   }),
-//   RESOURCE_ID: envalid.str({
-//     example: 'd197a05e-...',
-//     desc: 'The unique APP ID of the Azure Web API to obtain access to',
-//   }),
-//   RBAC_GROUP: envalid.str({
-//     example: 'API Group',
-//     desc:
-//       'The name of the group of users for whom access to this API is granted',
-//   }),
-//   // PORT: envalid.port({
-//   //   example: '3000',
-//   //   default: 3000,
-//   //   desc: 'The localhost port on which this sample would run',
-//   // }),
-// });
 
 class App {
   constructor() {
@@ -48,7 +11,6 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
-    // this.server.use(express.urlencoded());
   }
 
   routes() {
